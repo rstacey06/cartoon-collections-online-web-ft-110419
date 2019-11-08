c0c937b8b34c450137912f36f21066f155796e26
Burbage Stacey <rstacey06@gmail.com>
2019-11-08 13:22:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,5 +25,15 @@ long_planeteer_calls(calls)
 
 
 
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
+
+find_the_cheese(potentially_cheesy_items)
 
